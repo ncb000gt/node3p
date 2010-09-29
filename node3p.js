55e54ec03f996a2249cb908ff105769179a45549
Nick Campbell <nicholas.j.campbell@gmail.com>
2010-09-29 00:10:49 -0400

    Removed commentary. Split code into proper functions. Able to download more than one file. Also, the downloads are throttled based on MAX_DOWNLOADS, currently set to 5, for now. This will likely be a configuration option in the future.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- node3p.js
+++ node3p.js
@@ -10,21 +10,24 @@ var sys = require('sys')
 ,path = require('path')
 ,fshelp = require('./lib/fshelper.js');
 
+var MAX_DOWNLOADS = 5;
 var KEY = '\x29\xAB\x9D\x18\xB2\x44\x9E\x31';
 var IV = '\x5E\x72\xD7\x9A\x11\xB3\x4F\xEE';
 
 var saveRoot = process.argv[2];
 
 var amz_data = fs.readFileSync(process.argv[3], 'binary');
-sys.puts("data size: " + amz_data.length);
+//sys.puts("data size: " + amz_data.length);
 var b = new Buffer(amz_data.length);
 b.write(amz_data, 'binary');
 
 var decipher=(new crypto.Decipher).initiv("des-cbc",KEY,IV);
 var amz_xml = decipher.update(b64decode(b), 'binary', 'utf8');
 amz_xml += decipher['final']('utf8');
-sys.puts("xml: " + amz_xml);
-var links = [];
+//sys.puts("xml: " + amz_xml);
+var toDownload = [];
+var dlCount = 0;
+var canDownload = true;
 
 var response = xml2object.parseString(
   amz_xml,
@@ -49,34 +52,69 @@ var response = xml2object.parseString(
 	}
       }
       var track_url = url.parse(track.location.content, true);
-      var u = track_url.query.URL;
-      links.push({url: u, size: size, primary: primary, creator: track.creator.content, album: track.album.content, title: track.title.content });
+      var uri = track_url.query.URL;
+      var creator = track.creator.content;
+      var album = track.album.content;
+      var title = track.title.content;
+
+      //get ready to download
+      /*sys.puts('creator: ' + creator);
+      sys.puts('album: ' + album);
+      sys.puts('title: ' + title);*/
+      //sys.debug('saveRoot: '+saveRoot);
+      toDownload.push({uri: uri, primary: primary||creator, album: album, title: title});
     }
 
-    var link = links[0];
-    sys.puts('link: ' +sys.inspect(link));
-    var p = url.parse(link.url);
-    sys.puts('creator: ' + link.creator);
-    sys.puts('album: ' + link.album);
-    sys.puts('title: ' + link.title);
 
-    sys.debug('saveRoot: '+saveRoot);
-    var dirpath = path.join(saveRoot, primary, link.album);
-    var filepath = path.join(dirpath, link.title + '.mp3');
-
-    fshelp.mkdirs(dirpath, 0777, function(err) {
-		    if (err) throw err;
-		    fs.open(filepath, 'w', 666, function(err, fd) {
-			      request({uri: links[0].url, responseBodyStream: fs.createWriteStream(filepath)}, function (err, response, stream) {
-					if (err) sys.puts('there was a problem scotty');
-					stream.on('end', function() {
-						    fs.close(fd, function(err) {
-							       sys.puts('File has been written, try it out.');
-							     });
-						  });
-				      });
-			    });
-
-		  });
+    downloadFiles();
   }
 );
+
+function downloadFiles() {
+  if (toDownload.length > 0) {
+    if (dlCount < MAX_DOWNLOADS) {
+      var o = toDownload.pop();
+      sys.puts('primary: ' + o.primary);
+      sys.puts('album: ' + o.album);
+      sys.puts('title: ' + o.title);
+      dlCount++;
+      getFile(o.uri, o.primary, o.album, o.title);
+    }
+    setTimeout(downloadFiles, 1000);
+  } else {
+    sys.puts('No more files to enqueued for download. Finishing the download.');
+  }
+}
+
+function checkDL() {
+  sys.debug('checkDL - dlCount: ' + dlCount);
+  while (dlCount >= MAX_DOWNLOADS) {
+    setTimeout(function() {}, 1000);
+  }
+  return;
+}
+
+function getFile(uri, primary, album, title) {
+  var dirpath = path.join(saveRoot, primary, album);
+  var filepath = path.join(dirpath, title + '.mp3');
+  sys.puts("Writing '"+filepath+"'...");
+  fshelp.mkdirs(dirpath, 0777, function(err) {
+		  if (err) throw err;
+		  fs.open(filepath, 'w', 0755, function(err, fd) {
+			    if (err) throw err;
+			    var s = fs.createWriteStream(filepath);
+			    s.on('close', function(err) {
+				   if (err) throw err;
+				   fs.close(fd, function(err) {
+					      if (err) throw err;
+					      sys.puts("File '"+filepath+"' has been written, try it out.");
+					      dlCount--;
+					    });
+				 });
+			    request({uri: uri, responseBodyStream: s}, function (err, response, stream) {
+				      if (err) sys.puts('there was a problem scotty');
+				    });
+			  });
+
+		});
+};
